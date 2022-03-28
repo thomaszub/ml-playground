5c3c877a1b3d88e13840c3b3dd05725b69415bdd
Thomas Zub <thomas.zub@codecentric.de>
2022-03-28 17:07:30 +0200

    Renamed action to movement

--- Dynamic-Programming/world.py
+++ Dynamic-Programming/world.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from enum import Enum
 from typing import List, Tuple
-from action import (
+from movement import (
     Position,
     MoveAction,
     UpMoveAction,
