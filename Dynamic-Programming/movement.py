e2afb230077de72d5a74f69dfd784766ca158d2d
thomaszub <thomas.zub@outlook.de>
2022-03-31 17:12:06 +0200

    Made MoveAction implementation dataclasses

--- Dynamic-Programming/movement.py
+++ Dynamic-Programming/movement.py
@@ -9,6 +9,7 @@ class Position:
     Y: int
 
 
+@dataclass(frozen=True)
 class MoveAction(ABC):
     @abstractmethod
     def apply(self, position: Position) -> Position:
@@ -22,41 +23,17 @@ class UpMoveAction(MoveAction):
     def apply(self, position: Position) -> Position:
         return Position(position.X, position.Y + 1)
 
-    def __str__(self) -> str:
-        return "UP"
-
-    def __repr__(self) -> str:
-        return "UP"
-
 
 class DownMoveAction(MoveAction):
     def apply(self, position: Position) -> Position:
         return Position(position.X, position.Y - 1)
-
-    def __str__(self) -> str:
-        return "DOWN"
-
-    def __repr__(self) -> str:
-        return "DOWN"
 
 
 class LeftMoveAction(MoveAction):
     def apply(self, position: Position) -> Position:
         return Position(position.X - 1, position.Y)
 
-    def __str__(self) -> str:
-        return "LEFT"
-
-    def __repr__(self) -> str:
-        return "LEFT"
-
 
 class RightMoveAction(MoveAction):
     def apply(self, position: Position) -> Position:
         return Position(position.X + 1, position.Y)
-
-    def __str__(self) -> str:
-        return "RIGHT"
-
-    def __repr__(self) -> str:
-        return "RIGHT"
