f15731073793ce89e1ce630c18ace03591117c25
thomaszub <thomas.zub@outlook.de>
2022-03-31 20:19:04 +0200

    Added policy for manual action list

--- Dynamic-Programming/policy.py
+++ Dynamic-Programming/policy.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Callable
+from typing import Callable, Dict, List
 
 import numpy as np
 
@@ -35,3 +35,19 @@ class UniformRandomPolicy(Policy):
     def func(self, position: Position) -> Callable[[MoveAction], float]:
         possible_actions = self._world.possible_actions(position)
         return lambda x: 1.0 / len(possible_actions)
+
+
+class ManuelPolicy(Policy):
+    def __init__(
+        self, world: GridWorld, positionToAction: Dict[Position, List[MoveAction]]
+    ) -> None:
+        super().__init__(world)
+        self._positionToAction = positionToAction
+
+    def func(self, position: Position) -> Callable[[MoveAction], float]:
+        chosen_actions = self._positionToAction[position]
+        return (
+            lambda action: 1.0 / len(chosen_actions)
+            if action in chosen_actions
+            else 0.0
+        )
