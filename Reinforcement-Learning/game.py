1e79ed4e6d095f0ee2947dc37d2e253c4e515487
thomaszub <thomas.zub@outlook.de>
2022-04-06 21:17:24 +0200

    Added callback function

--- Reinforcement-Learning/game.py
+++ Reinforcement-Learning/game.py
@@ -1,10 +1,16 @@
 from copy import copy
+from typing import Callable
 
-from policy import Policy
 from environment.world import Field, GridWorld
+from policy import Policy
 
 
-def play(world: GridWorld, policy: Policy, start_field: Field):
+def play(
+    world: GridWorld,
+    policy: Policy,
+    start_field: Field,
+    new_state_callback: Callable[[Field, float, Field], None] = None,
+):
     trajectorie = []
 
     field = start_field
@@ -12,6 +18,8 @@ def play(world: GridWorld, policy: Policy, start_field: Field):
     while not game_ended:
         action = policy.sample(field)
         new_field, reward = world.move(field, action)
+        if new_state_callback != None:
+            new_state_callback(field, reward, new_field)
         trajectorie.append((copy(field), action, reward, new_field))
         field = new_field
         game_ended = new_field.type.is_terminal()
