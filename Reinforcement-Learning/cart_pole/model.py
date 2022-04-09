567d17cdfb7a8db152480c5c00bf276e089d1c38
thomaszub <thomas.zub@outlook.de>
2022-04-09 16:53:42 +0200

    Removed action encoder

--- Reinforcement-Learning/cart_pole/model.py
+++ Reinforcement-Learning/cart_pole/model.py
@@ -4,7 +4,6 @@ import gym
 import numpy as np
 from sklearn.kernel_approximation import RBFSampler
 from sklearn.linear_model import SGDRegressor
-from sklearn.preprocessing import OneHotEncoder
 
 
 class ActionModel(ABC):
@@ -24,31 +23,29 @@ class LinearRBFActionModel(ActionModel):
         action_space: gym.Space[int],
         learning_rate: float,
     ) -> None:
-        self._action_encoder = OneHotEncoder(categories=[[0, 1]])
-        self._action_encoder.fit(np.array([[action_space.sample()]]))
-        self._sampler = RBFSampler()
-        self._sampler.fit([observation_space.sample()])
-        self._model = SGDRegressor()
+        self._sampler = RBFSampler(n_components=4)
+        self._sampler.fit(
+            self._input(observation_space.sample(), action_space.sample())
+        )
+        self._model = SGDRegressor(learning_rate="constant", eta0=learning_rate)
         self._model.partial_fit(
-            self._input(observation_space.sample(), action_space.sample()),
+            self._sampled_input(observation_space.sample(), action_space.sample()),
             self._target(0.0),
         )
 
     def predict(self, observation: np.ndarray, action: int) -> float:
-        return self._model.predict(self._input(observation, action))
+        return self._model.predict(self._sampled_input(observation, action))
 
     def update(self, observation: np.ndarray, action: int, target: float) -> None:
-        self._model.partial_fit(self._input(observation, action), self._target(target))
+        self._model.partial_fit(
+            self._sampled_input(observation, action), self._target(target)
+        )
 
     def _input(self, observation: np.ndarray, action: int) -> np.ndarray:
-        return [
-            np.concatenate(
-                (
-                    observation,
-                    self._action_encoder.transform([[action]]).toarray().reshape(-1),
-                )
-            )
-        ]
+        return [np.concatenate((observation, [action]))]
+
+    def _sampled_input(self, observation: np.ndarray, action: int) -> np.ndarray:
+        return self._sampler.transform(self._input(observation, action))
 
     def _target(self, value: float):
         return np.array([value]).ravel()
