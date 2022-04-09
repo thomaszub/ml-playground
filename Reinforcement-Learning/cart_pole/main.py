4d3e7ef1c604ba8a8cacbb695b5e70b801cc6861
thomaszub <thomas.zub@outlook.de>
2022-04-09 12:33:03 +0200

    Loop rewritten

--- Reinforcement-Learning/cart_pole/main.py
+++ Reinforcement-Learning/cart_pole/main.py
@@ -1,4 +1,5 @@
 import gym
+from tqdm import trange
 
 from agent import Agent
 from model import LinearRBFActionModel
@@ -6,22 +7,27 @@ from model import LinearRBFActionModel
 
 def main() -> None:
     env = gym.make("CartPole-v1")
-
+    render = False
     agent = Agent(
         LinearRBFActionModel(env.observation_space, env.action_space, 0.1), 0.9, 0.1
     )
-    for it in range(0, 100):
-        steps = 0
-        done = False
-        observation = env.reset()
-        while not done:
-            steps += 1
-            env.render()
-            action = agent.sample(observation)
-            new_observation, reward, done, info = env.step(action)
-            agent.train(observation, action, reward, new_observation, done)
-            observation = new_observation
-        print(f"Try: {it}, steps: {steps}")
+
+    with trange(0, 200, desc="Iteration") as titer:
+        for _ in titer:
+            steps = 0
+            done = False
+            observation = env.reset()
+            while not done:
+                steps += 1
+                if render:
+                    env.render()
+                action = agent.sample(observation)
+                new_observation, reward, done, info = env.step(action)
+                agent.train(observation, action, reward, new_observation, done)
+                observation = new_observation
+
+            titer.set_postfix(steps=steps)
+            titer.update()
 
     env.close()
 
