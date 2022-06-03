f15d1cc771e13a214a861bffdac78bce8d6c5c19
Thomas Zub <thomas.zub@codecentric.de>
2022-06-03 17:09:12 +0200

    Renaming, added function for datasets

--- MNIST/data.py
+++ MNIST/data.py
@@ -4,13 +4,12 @@ from torchvision import datasets
 from torchvision.transforms import Lambda, ToTensor
 
 
-def get_data(batch_size):
+def get_datasets():
     one_hot_encoding = Lambda(
         lambda label: torch.zeros(10, dtype=torch.float).scatter_(
             dim=0, index=torch.tensor(label), value=1
         )
     )
-
     data_train = datasets.MNIST(
         "data",
         train=True,
@@ -25,6 +24,11 @@ def get_data(batch_size):
         transform=ToTensor(),
         target_transform=one_hot_encoding,
     )
+    return data_train, data_test
+
+
+def get_dataloaders(batch_size):
+    data_train, data_test = get_datasets()
     loader_train = DataLoader(dataset=data_train, batch_size=batch_size, shuffle=True)
     loader_test = DataLoader(dataset=data_test, batch_size=batch_size, shuffle=True)
     return loader_train, loader_test
